d1aa619dbb2f0cc58147d8af7b06b3fb00ec5cc1
Raha Heidari <heidariraha761@gmail.com>
2024-12-14 12:55:48 -0800

    js

---
+++ JS-13-Other.js
@@ -0,0 +1,29 @@
+// ? Question-1:select section counter by class name(.cards__card__img__icon) and save in variable "counterEl":
+// !Answer:
+
+const counterEl = document.querySelector(".cards__card__img__icon")
+
+// ? Question-2:select item counter by id(#counter) and save in variable "counter":
+// !Answer:
+  
+const counter = document.getElementById("counter")
+
+// ? Question-3:select section (.cards) and save in variable "card":
+// !Answer:
+
+const card = document.querySelector(".cards")
+
+// ? Question-4:select section (.cards__card) and save in variable "cards":
+// !Answer:
+
+const cards = document.querySelector(".cards__card")
+
+// ? Question-5:select button in class cards_card_footer and save in variable "btnEl":
+// !Answer
+ 
+const btnEl = document.querySelector(".cards_card_footer button")
+
+// ? Question-6:select the img that has attribute "man" and show in console:
+// !Answer
+const img = document.getElementsByTagName("img")
+console.log(img);
